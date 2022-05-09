d5871aa071271b9bbd15244f6df8f46b944ab69a
mishapadidar <map454@cornell.edu>
2022-05-09 16:17:57 -0500

    set up ex1 for tracking a blob of probability mass through space

--- stella/ex1_track_blob.py
+++ stella/ex1_track_blob.py
@@ -2,6 +2,14 @@ from stella import *
 import numpy as np
 from bfield import load_field,compute_rz_bounds,compute_plasma_volume,make_surface_classifier
 
+"""
+Track a blob of probability mass through space.
+
+We initialize a blob of probability mass spatially with
+parallel velocities distributed near v0, so that vperp is nearly
+zero.
+"""
+
 # load constants
 PROTON_MASS = 1.67262192369e-27  # kg
 NEUTRON_MASS = 1.67492749804e-27  # kg
@@ -11,7 +19,8 @@ FUSION_ALPHA_PARTICLE_ENERGY = 3.52e6 *ONE_EV # Ekin
 FUSION_ALPHA_SPEED_SQUARED = 2*FUSION_ALPHA_PARTICLE_ENERGY/ALPHA_PARTICLE_MASS
 
 # load the plasma volume, classifier and bfield
-ntheta=nphi=64
+nfp = 2 # number field periods
+ntheta=nphi=128
 plasma_vol = compute_plasma_volume(ntheta=ntheta,nphi=nphi)
 classifier = make_surface_classifier(ntheta=ntheta,nphi=nphi)
 bs = load_field(ntheta=ntheta,nphi=nphi)
@@ -25,32 +34,31 @@ rmin = max(rmin - 0.3*delta_r,0.0)
 delta_z = zmax - zmin 
 zmax = zmax + 0.3*delta_z
 zmin = zmin - 0.3*delta_z
-
-"""
-Set vperp=0, so that vpar=v is conserved. Then we 
-mesh vpar with a single point, and our density
-is a delta function in the vpar direction.
-
-Since we cannot computationally represent a delta function,
-we work with a degenerate density that integrates
-to zero.
-"""
-vpar0_lb = np.sqrt(FUSION_ALPHA_SPEED_SQUARED)
-vpar0_ub = np.sqrt(FUSION_ALPHA_SPEED_SQUARED)
+# set discretization sizes
+n_r = 64
+n_phi = 64
+n_z = 64
+n_vpar = 4
+dt = 1e-8
+tmax = 1e-6
+integration_method='midpoint'
+# set the initial density bounds for vpar
+vpar0_lb = 0.99*np.sqrt(FUSION_ALPHA_SPEED_SQUARED)
+vpar0_ub = 1.0*np.sqrt(FUSION_ALPHA_SPEED_SQUARED)
+# set the vpar grid bounds
 vparmin = vpar0_lb
 vparmax = vpar0_ub
-n_vpar = 1 # Only 1 vpar
-prob0 = 1/plasma_vol
-
-nfp = 2 # number field periods
-# set discretization sizes
-n_r = 10
-n_phi = 10
-n_z = 10
-dt = 1e-7
-tmax = 1e-5
-integration_method='midpoint'
-
+# set the initial density bounds for r,phi,z
+r0_lb = (rmin+rmax)/2 - 2*(rmax-rmin)/n_r
+r0_ub = (rmin+rmax)/2 + 2*(rmax-rmin)/n_r
+phi0_lb = np.pi/2 - np.pi/16
+phi0_ub = np.pi/2 + np.pi/16
+z0_lb = (zmin+zmax)/2 - 2*(zmax-zmin)/n_z
+z0_ub = (zmin+zmax)/2 + 2*(zmax-zmin)/n_z
+# compute the initial state volume
+vpar_vol = vpar0_ub - vpar0_lb
+spatial_vol = 0.5*(phi0_ub-phi0_lb)*(z0_ub-z0_lb)*(r0_ub**2 - r0_lb**2)
+prob0 = 1/spatial_vol/vpar_vol
 
 def cyl_to_cart(r_phi_z):
   """ cylindrical to cartesian coordinates 
@@ -68,20 +76,23 @@ def u0(X):
   """ 
   u0(r,phi,z,vpar) 
   input: X, 2d array of points in cylindrical (r,phi,z,vpar)
-
-  uniform distribution over the plasma, and [vpar0_lb,vpar0_ub]
   """
   xyz = cyl_to_cart(X[:,:-1])
   c= np.array([classifier.evaluate(x.reshape((1,-1)))[0].item() for x in xyz])
   idx_feas = c >= 0
+  idx_feas = np.logical_and(idx_feas, X[:,0] >= r0_lb)
+  idx_feas = np.logical_and(idx_feas, X[:,0] <= r0_ub)
   idx_feas = np.logical_and(idx_feas, X[:,-1] >= vpar0_lb)
   idx_feas = np.logical_and(idx_feas, X[:,-1] <= vpar0_ub)
+  idx_feas = np.logical_and(idx_feas, X[:,1] >= phi0_lb)
+  idx_feas = np.logical_and(idx_feas, X[:,1] <= phi0_ub)
+  idx_feas = np.logical_and(idx_feas, X[:,2] >= z0_lb)
+  idx_feas = np.logical_and(idx_feas, X[:,2] <= z0_ub)
   prob = np.zeros(len(xyz))
   # det(Jac) = r
   det_jac = X[:,0][idx_feas]
   prob[idx_feas] = prob0*det_jac
   return prob
-
 
 def bfield(xyz):
   # add zero to shut simsopt up
@@ -98,4 +109,6 @@ solver = STELLA(u0,bfield,gradAbsB,
     vparmin,vparmax,n_vpar,
     dt,tmax,integration_method)
 
-solver.solve()
+#solver.startup()
+#solver.write_spatial_marginal_vtk(tau=0)
+solver.solve(classifier=classifier)
