a6e99865ce3a3977b98ca5ee81f79c629c99cc3d
mishapadidar <map454@cornell.edu>
2022-05-12 13:34:56 -0400

    adapted coil computation to run for input.new_QA_scaling by increasing the initial coil radii. New method to scale coil currents based on the toroidal flux. in all methods that involve saving or loading coils: the coil currents are all unfixed.

--- stella/bfield.py
+++ stella/bfield.py
@@ -9,21 +9,21 @@ from simsopt.geo.curveobjectives import CurveLength, CurveCurveDistance
 from simsopt.geo.plot import plot
 
 
-def write_field(filename = "./input.LandremanPaul2021_QA",outpath="bs.LandremanPaul2021_QA",nphi=64,ntheta=64):
+def write_field(vmec_input = "./input.new_QA_scaling",bs_path="bs.new_QA_scaling",nphi=64,ntheta=64):
   """
   Compute the Landreman Paul coils and BiotSavart field, then
   save it.
   """
-  s = SurfaceRZFourier.from_vmec_input(filename, range="field period", nphi=nphi, ntheta=ntheta)
+  s = SurfaceRZFourier.from_vmec_input(vmec_input, range="field period", nphi=nphi, ntheta=ntheta)
   
   # Number of unique coil shapes:
   ncoils = 4
   
   # Major radius for the initial circular coils:
-  R0 = 1.0
+  R0 = 11.0
   
   # Minor radius for the initial circular coils:
-  R1 = 0.5
+  R1 = 2.5
   
   # Number of Fourier modes describing each Cartesian component of each coil:
   order = 5
@@ -67,18 +67,22 @@ def write_field(filename = "./input.LandremanPaul2021_QA",outpath="bs.LandremanP
   
   B_dot_n = np.sum(bs.B().reshape((nphi, ntheta, 3)) * s.unitnormal(), axis=2)
   print('Final max B dot n:', np.max(B_dot_n))
+
+  # unfix currents before saving
+  bs.coils[0].current.unfix_all()
+  print(len(bs.x))
   
   # run some computation / optimization
-  np.savetxt(outpath,bs.x)
+  np.savetxt(bs_path,bs.x)
   return bs
 
 
-def load_field(filename = "./input.LandremanPaul2021_QA",bs_path="./bs.LandremanPaul2021_QA",nphi=64,ntheta=64):
+def load_field(vmec_input = "./input.new_QA_scaling",bs_path="./bs.new_QA_scaling",nphi=64,ntheta=64):
   """
   Load the BiotSavart field for the LandremanPaul coils.
   """
   
-  s = SurfaceRZFourier.from_vmec_input(filename, range="field period", nphi=nphi, ntheta=ntheta)
+  s = SurfaceRZFourier.from_vmec_input(vmec_input, range="field period", nphi=nphi, ntheta=ntheta)
   
   # Number of unique coil shapes:
   ncoils = 4
@@ -95,7 +99,7 @@ def load_field(filename = "./input.LandremanPaul2021_QA",bs_path="./bs.Landreman
   base_curves = create_equally_spaced_curves(ncoils, s.nfp, stellsym=True, R0=R0, R1=R1, order=order)
   base_currents = [Current(1e5) for i in range(ncoils)]
   
-  base_currents[0].fix_all()
+  #base_currents[0].fix_all()
   
   coils = coils_via_symmetries(base_curves, base_currents, s.nfp, True)
   
@@ -109,34 +113,103 @@ def load_field(filename = "./input.LandremanPaul2021_QA",bs_path="./bs.Landreman
   bs.x = np.loadtxt(bs_path)
   return bs
 
-def compute_rz_bounds(filename = "./input.LandremanPaul2021_QA",bs_path="./bs.LandremanPaul2021_QA",nphi=64,ntheta=64):
+def compute_rz_bounds(vmec_input = "./input.new_QA_scaling",nphi=64,ntheta=64):
   # load the surface to get appropriate ranges
-  s = SurfaceRZFourier.from_vmec_input(filename, range="field period", nphi=nphi, ntheta=ntheta)
+  s = SurfaceRZFourier.from_vmec_input(vmec_input, range="field period", nphi=nphi, ntheta=ntheta)
   rs = np.linalg.norm(s.gamma()[:, :, 0:2], axis=2)
   zs = s.gamma()[:, :, 2]
   return np.min(rs),np.max(rs), np.min(zs),np.max(zs)
 
-def compute_plasma_volume(filename = "./input.LandremanPaul2021_QA",nphi=64,ntheta=64):
-  s = SurfaceRZFourier.from_vmec_input(filename, range="field period", nphi=nphi, ntheta=ntheta)
+def compute_plasma_volume(vmec_input = "./input.new_QA_scaling",nphi=64,ntheta=64):
+  s = SurfaceRZFourier.from_vmec_input(vmec_input, range="field period", nphi=nphi, ntheta=ntheta)
   return s.volume()
 
-def make_surface_classifier(filename = "./input.LandremanPaul2021_QA",nphi=64,ntheta=64):
+def make_surface_classifier(vmec_input = "./input.new_QA_scaling",nphi=64,ntheta=64):
   from simsopt.field.tracing import SurfaceClassifier
-  s = SurfaceRZFourier.from_vmec_input(filename, range="field period", nphi=nphi, ntheta=ntheta)
+  s = SurfaceRZFourier.from_vmec_input(vmec_input, range="field period", nphi=nphi, ntheta=ntheta)
   sc_particle = SurfaceClassifier(s, h=0.1, p=2)
   return sc_particle
 
+def rescale_coil_currents(vmec_input = "./input.new_QA_scaling",bs_path="./bs.new_QA_scaling",nphi=64, ntheta=64,target_tflux=5.048228826746e1,overwrite=False):
+  """
+  Rescale coil currents based on the target toroidal flux.
+
+  vmec_input: a vmec input file
+  bs_path: path to coils corresponding to the vmec input
+  nphi,ntheta: toroidal and poloidal discretizations of plasma
+  target_toroidal_flux: typically the toroidal flux for the vmec_input, or another toroidal
+    flux that you want to scale the currents based on
+  overwrite: bool, overwrite the bs_path
+  """
+  from simsopt.geo.surfaceobjectives import ToroidalFlux
+  s = SurfaceRZFourier.from_vmec_input(vmec_input, range="field period", nphi=nphi, ntheta=ntheta)
+  bs = load_field(vmec_input = vmec_input,bs_path=bs_path,nphi=nphi,ntheta=ntheta)
+
+  ncoils = 4
+
+  tflux = ToroidalFlux(s,bs)
+  tfluxJ = tflux.J()
+  print('initial tflux',tfluxJ)
+
+  # scale the coil currents
+  scale_factor = target_tflux/tfluxJ
+
+  #print('scale factor',scale_factor)
+  # set the coils currents
+  for coil in bs.coils[:ncoils]:
+    coil.current.unfix_all()
+    #print(coil.current.x)
+    coil.current.x *=scale_factor
+    #print(coil.current.x)
+    #print("")
+
+  tflux = ToroidalFlux(s,bs)
+  tfluxJ = tflux.J()
+  print('new tflux',tfluxJ)
+
+
+  ## print coil B normal
+  #bs.set_points(s.gamma().reshape((-1, 3)))
+  #print(bs.B())
+  #B_dot_n = np.sum(bs.B().reshape((nphi,ntheta, 3)) * s.unitnormal(), axis=2)
+  #print('max B dot n:', np.max(B_dot_n))
+
+  if overwrite:
+    np.savetxt(bs_path,bs.x)
+  return bs
+
 if __name__=="__main__":
+
   # make the field and save it
-  write_field()
+  write_field(vmec_input="input.new_QA_scaling", bs_path="bs.new_QA_scaling",ntheta=128,nphi=128)
+
+  # scale the coil currents to reactor strength
+  print('')
+  rescale_coil_currents(ntheta=128,nphi=128,overwrite=True)
+
   # load it
-  field = load_field()
+  bs = load_field()
+
   # eval it
-  field.set_points(np.array([[0.5, 0.5, 0.1], [0.1, 0.1, -0.3]]))
-  print(field.B())
+  nphi=ntheta=128
+  s = SurfaceRZFourier.from_vmec_input("input.new_QA_scaling", range="field period",ntheta=ntheta,nphi=nphi)
+  bs.set_points(s.gamma().reshape((-1, 3)))
+  B_dot_n = np.sum(bs.B().reshape((nphi,ntheta, 3)) * s.unitnormal(), axis=2)
+  print('')
+  print(bs.B())
+  print('max B dot n:', np.max(B_dot_n))
+
+  # get bounds
   rmin,rmax,zmin,zmax = compute_rz_bounds()
+  print('')
+  print('r and z bounds')
   print(rmin,rmax,zmin,zmax)
+
+  # get volume
   vol = compute_plasma_volume()
-  print(vol)
+  print('')
+  print('volume',vol)
+
+  # make a surface classifier
   classifier = make_surface_classifier()
 
