d41bb45340a7911e0e6e8e863f0a0febb3b85b36
mishapadidar <map454@cornell.edu>
2022-05-14 12:18:50 -0400

    adding new stopping criteria, documentation, and fixed the inital particle distribution to be uniform

--- trace/ex1_track_blob.py
+++ trace/ex1_track_blob.py
@@ -2,6 +2,7 @@
 import numpy as np
 import sys
 from guiding_center_eqns import *
+from simsopt.field.tracing import trace_particles, LevelsetStoppingCriterion,ToroidalTransitStoppingCriterion
 sys.path.append("../utils")
 sys.path.append("../stella")
 from bfield import load_field,compute_rz_bounds,compute_plasma_volume,make_surface_classifier
@@ -12,13 +13,25 @@ Track a blob of probability mass through space.
 
 The blob is initialized with a uniform distribution in 
 Cartesian space over a small block in r,phi,z,vpar space.
+
+We can trace with either simsopt or with the euler or midpoint
+method. If tracing with euler or midpoint we can choose
+to turn the drift terms off or on in the guiding center equations.
+
+Depending on if we trace with simsopt or one of our methods the 
+vtk files look a bit different. With our method the pvd file will
+play a movie that moves forward in time. When tracing with
+simsopt each frame of the pvd will be an individual particles 
+trajectory. In either case, make a glyph of the pvd with spheres
+to visualize.
 """
-n_particles = 20
-tmax = 1e-4
-dt = 1e-10
-n_skip = 1000
-trace_simsopt = True
-method = 'euler'
+n_particles = 100
+tmax = 1e-3
+dt = 1e-7
+n_skip = 10
+trace_simsopt = False
+method = 'midpoint' # euler or midpoint
+include_drifts = True
 
 # seed
 #np.random.seed(0)
@@ -77,18 +90,14 @@ vpar_vol = vpar0_ub - vpar0_lb
 spatial_vol = 0.5*(phi0_ub-phi0_lb)*(z0_ub-z0_lb)*(r0_ub**2 - r0_lb**2)
 prob0 = 1/spatial_vol/vpar_vol
 
-# make a guiding center object
-GC = GuidingCenter(bfield,gradAbsB,include_drifts=False)
-
 # sample phi,z,vpar uniformly at random
 # sample r using the inverse transform r = F^{-1}(U)
-# where the CDF inverse if F^{-1}(u) = sqrt(2u/D) and
+# where the CDF inverse if F^{-1}(u) = sqrt(2u/D + r0_lb^2) 
 # D = 2/(r0_ub^2 - r0_lb^2)
-# TODO: initial distribution in R is incorrect!
-#U = np.random.uniform(0,1,n_particles)
-#D = 2.0/(r0_ub**2 - r0_lb**2)
-#R = np.sqrt(2*U/D)
-R = np.random.uniform(r0_lb,r0_ub,n_particles)
+U = np.random.uniform(0,1,n_particles)
+D = 2.0/(r0_ub**2 - r0_lb**2)
+R = np.sqrt(2*U/D + r0_lb**2)
+#R = np.random.uniform(r0_lb,r0_ub,n_particles)
 Phi = np.random.uniform(phi0_lb,phi0_ub,n_particles)
 Z = np.random.uniform(z0_lb,z0_ub,n_particles)
 Vpar = np.random.uniform(vpar0_lb,vpar0_ub,n_particles)
@@ -97,16 +106,18 @@ X = np.vstack((R,Phi,Z,Vpar)).T
 # vtk writer
 vtk_writer = vtkClass.VTK_XML_Serial_Unstructured()
 
+# make a guiding center object
+GC = GuidingCenter(bfield,gradAbsB,include_drifts=include_drifts)
+
 if trace_simsopt:
   """
   Trace particles with simsopt. Write each particle trajectory to its own vtk file.
   We use the color atribute to encode time.
   """
-  from simsopt.field.tracing import trace_particles, LevelsetStoppingCriterion
   # load the surface classifier
   classifier = make_surface_classifier(vmec_input=vmec_input, ntheta=ntheta,nphi=nphi)
   xyz_inits = GC.cyl_to_cart(X[:,:-1])
-  stopping_criteria=[LevelsetStoppingCriterion(classifier.dist)]
+  stopping_criteria=[LevelsetStoppingCriterion(classifier.dist),ToroidalTransitStoppingCriterion(100, True)]
   for ii in range(n_particles):
     print("tracing particle ",ii)
     xyz = xyz_inits[ii].reshape((1,-1))
@@ -124,12 +135,13 @@ else:
   """
   Trace all particle simultaneously. Each vtk file contains a time slice.
   """
+
   times = np.arange(0,tmax,dt)
   for n_step,t in enumerate(times):
-    print("t = ",t)
     # make a paraview file
     if n_step % n_skip == 0:
-      vtkfilename = f"./plot_data/trace_{n_step:09d}.vtu"
+      print("t = ",t)
+      vtkfilename = f"./plot_data/trace_{method}_{n_step:09d}.vtu"
       xyz = GC.cyl_to_cart(X[:,:-1])
       vtk_writer.snapshot(vtkfilename, xyz[:,0],xyz[:,1],xyz[:,2])
     # take a step
@@ -144,6 +156,6 @@ else:
     X = np.copy(Xtp1)
 
   # make the pvd
-  pvd_filename = f"./plot_data/trajectories.pvd"
+  pvd_filename = f"./plot_data/trajectories_{method}.pvd"
 vtk_writer.writePVD(pvd_filename)
   
