51c9a61f994ff32c7dd41ae71268083b6a885242
mishapadidar <map454@cornell.edu>
2022-09-27 19:58:35 -0400

    slimmed down the file to just the tracer

--- trace/trace_cartesian.py
+++ trace/trace_cartesian.py
@@ -1,57 +1,11 @@
 import numpy as np
-from simsopt.geo.surfacerzfourier import SurfaceRZFourier
 from simsopt.field.tracing import trace_particles, LevelsetStoppingCriterion
 import sys
-from guiding_center_eqns_cartesian import *
-sys.path.append("../stella")
-from bfield import load_field,compute_rz_bounds,compute_plasma_volume,make_surface_classifier
 sys.path.append("../utils")
-from coords import cyl_to_cart,cart_to_cyl
-from concentricSurfaceClassifier import concentricSurfaceClassifier
 from constants import *
-from grids import loglin_grid
-import vtkClass
 
 
-def uniform_sampler(surf,classifier):
-  """
-  Sample uniformly from the plasma.
-  """
-  # set the bounds
-  rmin,rmax,zmin,zmax = compute_rz_bounds(surf)
-  lb_vpar = -1.0*np.sqrt(FUSION_ALPHA_SPEED_SQUARED)
-  ub_vpar = 1.0*np.sqrt(FUSION_ALPHA_SPEED_SQUARED)
-  lb_r,ub_r = rmin,rmax
-  lb_phi,ub_phi = 0,2*np.pi
-  lb_z,ub_z = zmin,zmax
-  
-  # do rejection sampling
-  X = np.zeros((0,4)) # [r,phi,z, vpar]
-  while len(X) < n_particles:
-    # sample r using the inverse transform r = F^{-1}(U)
-    # where the CDF inverse if F^{-1}(u) = sqrt(2u/D + r0_lb^2) 
-    # D = 2/(r0_ub^2 - r0_lb^2)
-    U = np.random.uniform(0,1)
-    D = 2.0/(ub_r**2 - lb_r**2)
-    r = np.sqrt(2*U/D + lb_r**2)
-    # sample uniformly from phi
-    phi = np.random.uniform(lb_phi,ub_phi)
-    # sample uniformly from z
-    z = np.random.uniform(lb_z,ub_z)
-    cyl  = np.array([r,phi,z])
-    xyz = cyl_to_cart(np.atleast_2d(cyl))
-    # check if particle is in plasma
-    if classifier.evaluate(np.atleast_2d(xyz)) > 0:
-      vpar = np.random.uniform(lb_vpar,ub_vpar,1)
-      point = np.append(xyz.flatten(),vpar)
-      X =np.vstack((X,point)) # [x,y,z, vpar]
-
-  return xyz
-
-  
-
-
-def compute_losses(X,bs,classifier,tmax):
+def compute_loss_times(X,bs,classifier,tmax):
   
   """
   Sample from the initial distribution
@@ -113,23 +67,3 @@ def compute_losses(X,bs,classifier,tmax):
   return exit_states,exit_times
   
 
-"""
-Compute particle losses for a vmec surface
-"""
-np.random.seed(0)
-
-n_particles = 10
-tmax = 1e-5
-vmec_input = "../vmec_input_files/input.new_QA_scaling"
-
-# load the initial surface
-nphi=ntheta=128
-surf = SurfaceRZFourier.from_vmec_input(vmec_input, range="field period", nphi=nphi, ntheta=ntheta)
-# build coils
-bs = compute_biot_savart_field(surf)
-# scale the coil currents to reactor strength
-bs = rescale_coil_currents(surf,bs)
-# load the surface classifier
-nphi=ntheta=512
-surf = SurfaceRZFourier.from_vmec_input(vmec_input, range="full torus", nphi=nphi, ntheta=ntheta)
-classifier = make_surface_classifier(vmec_input=vmec_input, rng="full torus",ntheta=ntheta,nphi=nphi)
