8d67ef491200205d1c0bf47cf33c9edd4cc3daed
Misha Padidar <map454@cornell.edu>
2022-10-04 16:10:43 -0400

    added an eval wrapper

---
+++ utils/eval_wrapper.py
@@ -0,0 +1,34 @@
+
+import numpy as np
+
+class eval_wrapper:
+  """
+  Class for wrapping a function call so that we can 
+  save evaluations.
+  F: function handle
+  dim_x: function input dimension
+  dim_F: function output dimension
+  Usage:
+    F = lambda x: [np.linalg.norm(x),np.sum(x)]
+    dim_x = 3
+    dim_F = 2
+    func = eval_wrapper(f,dim_x,dim_F)
+    # call the function
+    func(np.random.randn(dim_x))
+    # check the history
+    print(func.X)
+    print(func.FX)
+  """
+
+  def __init__(self,F,dim_x,dim_F):
+    self.dim_x = dim_x
+    self.dim_F = dim_F
+    self.X = np.zeros((0,dim_x))
+    self.FX = np.zeros((0,dim_F))
+    self.func = F
+
+  def __call__(self,xx):
+    ff = self.func(xx)
+    self.X = np.append(self.X,[xx],axis=0)
+    self.FX = np.append(self.FX,[ff],axis=0)
+    return ff
