cb47b6374b2a1b809d66f12ffdf98d5a8611d67e
Misha Padidar <map454@cornell.edu>
2022-11-26 11:29:58 -0500

    set up dynamic resolution

--- experiments/min_quasisymmetry/optimize.py
+++ experiments/min_quasisymmetry/optimize.py
@@ -21,25 +21,15 @@ throughout the volume.
 print("Running 2_Intermediate/QH_fixed_resolution.py")
 print("=============================================")
 
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-
-mpi = MpiPartition(size)
-max_mode = 1
+mpi = MpiPartition()
+#max_mode = 1
+largest_mode = 4
 input_config = "input.nfp4_QH_cold_high_res"
 vmec_input = "../../vmec_input_files/" + input_config
 outfile = input_config + "_quasysymmetry_opt"
 
 vmec = Vmec(vmec_input, mpi=mpi,keep_all_files=False,verbose=False)
-
-# Define parameter space:
 surf = vmec.boundary
-surf.fix_all()
-surf.fixed_range(mmin=0, mmax=max_mode,
-                 nmin=-max_mode, nmax=max_mode, fixed=False)
-surf.fix("rc(0,0)")  # Major radius
-
-print('Parameter space:', surf.dof_names)
 
 # Configure quasisymmetry objective:
 qs = QuasisymmetryRatioResidual(vmec,
@@ -50,17 +40,44 @@ qs = QuasisymmetryRatioResidual(vmec,
 prob = LeastSquaresProblem.from_tuples([(vmec.aspect, 7, 1),
                                         (qs.residuals, 0, 1)])
 
-print("Quasisymmetry objective before optimization:", qs.total())
-print("Total objective before optimization:", prob.objective())
+if mpi.proc0_world:
+    print("Quasisymmetry objective before optimization:", qs.total())
+    print("Total objective before optimization:", prob.objective())
 
-least_squares_mpi_solve(prob, mpi, grad=True, rel_step=1e-5, abs_step=1e-8)
+#least_squares_mpi_solve(prob, mpi, grad=True, rel_step=1e-5, abs_step=1e-8)
 
-print("Final aspect ratio:", vmec.aspect())
-print("Quasisymmetry objective after optimization:", qs.total())
-print("Total objective after optimization:", prob.objective())
+for step in range(largest_mode):
+    max_mode = step + 1
 
-# write the data to a file
-vmec.write_input(outfile)
+    # VMEC's mpol & ntor will be 3, 4, 5, 6:
+    vmec.indata.mpol = 3 + step
+    vmec.indata.ntor = vmec.indata.mpol
+
+    if mpi.proc0_world:
+        print("")
+        print("Beginning optimization with max_mode =", max_mode, \
+              ", vmec mpol=ntor=", vmec.indata.mpol, \
+              ". Previous vmec iteration = ", vmec.iter)
+
+    # Define parameter space:
+    surf.fix_all()
+    surf.fixed_range(mmin=0, mmax=max_mode,
+                     nmin=-max_mode, nmax=max_mode, fixed=False)
+    surf.fix("rc(0,0)") # Major radius
+
+    # Carry out the optimization for this step:
+    least_squares_mpi_solve(prob, mpi, grad=True)
+
+    if mpi.proc0_world:
+        print("Done optimization with max_mode =", max_mode, \
+              ". Final vmec iteration = ", vmec.iter)
+
+        print("Final aspect ratio:", vmec.aspect())
+        print("Quasisymmetry objective after optimization:", qs.total())
+        print("Total objective after optimization:", prob.objective())
+    
+    # write the data to a file
+    vmec.write_input(outfile + f"_max_mode_{max_mode}")
 
 print("End of 2_Intermediate/QH_fixed_resolution.py")
 print("============================================")
