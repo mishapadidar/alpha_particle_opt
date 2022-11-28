f70afe4906e769b27d795c2f98b1da628fd54d78
Misha Padidar <map454@cornell.edu>
2022-11-28 16:02:51 -0500

    changes to post process to get ready for post processing

--- experiments/min_quasisymmetry/post_process.py
+++ experiments/min_quasisymmetry/post_process.py
@@ -2,22 +2,28 @@ import numpy as np
 import pickle
 import sys
 import time
+import glob
+from mpi4py import MPI
 sys.path.append("../../utils")
 sys.path.append("../../trace")
 sys.path.append("../../sample")
 from trace_boozer import TraceBoozer
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 
 minor_radius = 1.7
 major_radius = 8.0*1.7
 target_volavgB = 5.0
 tmax = 1e-2
-n_particles = 1000
+n_particles = 10000
 
-filelist = glob.glob("data_nfp2_QA_cold_high_res_max_mode_*_quasisymmetry_opt.pickle")
-#filelist = glob.glob("data_nfp4_QH_cold_high_res_max_mode_*_quasisymmetry_opt.pickle")
+#filelist = glob.glob("data_nfp2_QA_cold_high_res_max_mode_*_quasisymmetry_opt.pickle")
+filelist = glob.glob("data_nfp4_QH_cold_high_res_max_mode_*_quasisymmetry_opt.pickle")
 
-for infile in file_list 
-  print("processing",infile)
+for infile in filelist:
+  if rank == 0:
+    print("")
+    print("processing",infile)
 
   indata = pickle.load(open(infile,"rb"))
   max_mode = indata['max_mode']
@@ -45,19 +51,26 @@ for infile in file_list
   # compute the mirror ratio
   field,bri = tracer.compute_boozer_field(x0)
   modB = tracer.compute_modB(field,bri,ns=64,ntheta=64,nphi=64)
-  print('mirror ratio',np.max(modB)/np.min(modB))
+  if rank == 0:
+    print('mirror ratio',np.max(modB)/np.min(modB))
   
   # tracing points
   stz_inits,vpar_inits = tracer.sample_volume(n_particles)
   
   t0  = time.time()
-  print('tracing')
+  if rank == 0:
+    print('tracing')
+
   c_times = tracer.compute_confinement_times(x0,stz_inits,vpar_inits,tmax)
-  print('time',time.time() - t0)
-  print('mean',np.mean(c_times))
-  print('loss fraction',np.mean(c_times < tmax))
+
+  if rank == 0:
+    print('time',time.time() - t0)
+    print('mean',np.mean(c_times))
+    print('loss fraction',np.mean(c_times < tmax))
   
 
   # dump a pickle file
-  indata['c_times'] = c_times
-  #pickle.dump(indata,open(infilename,"wb"))
+  if rank == 0:
+    indata['c_times'] = c_times
+    indata['tmax'] = tmax
+    pickle.dump(indata,open(infilename,"wb"))
