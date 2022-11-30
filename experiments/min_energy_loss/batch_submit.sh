423773e5805f20297bb92402df46e2c012beaae5
Misha Padidar <map454@cornell.edu>
2022-11-30 13:56:33 -0500

    minor chagnes to batch submit

--- experiments/min_energy_loss/batch_submit.sh
+++ experiments/min_energy_loss/batch_submit.sh
@@ -9,32 +9,41 @@ SURFS=('0.3' '0.5' 'full')
 OBJECTIVE='mean_energy'
 #OBJECTIVE='mean_time'
 
-METHOD="pdfo" # pdfo, nelder, snobfit, diff_evol, sidpsm
+#METHOD="bobyqa" # pdfo, nelder, snobfit, diff_evol, sidpsm
+METHOD="cobyla" # pdfo, nelder, snobfit, diff_evol, sidpsm
+
 MAXMODE=1
 
-#VMEC="nfp2_QA_cold_high_res"
-VMEC="nfp2_QA_cold_high_res_mirror_feas"
-#VMEC="nfp4_QH_warm_high_res"
-#VMEC="nfp4_QH_cold_high_res"
-
-WARM=("None" "None" "None") # None or filename
-#WARM=("../data/data_opt_nfp4_QH_cold_high_res_mean_energy_grid_surface_0.3_tmax_0.001_pdfo_mmode_2.pickle" 
-#      "../data/data_opt_nfp4_QH_cold_high_res_mean_energy_grid_surface_0.5_tmax_0.001_pdfo_mmode_2.pickle" 
-#      "../data/data_opt_nfp4_QH_cold_high_res_mean_energy_grid_surface_0.7_tmax_0.001_pdfo_mmode_2.pickle" 
-#      "../data/data_opt_nfp4_QH_cold_high_res_mean_energy_grid_surface_full_tmax_0.001_pdfo_mmode_2.pickle" 
-#      )
-
-NS=10
-NTHETA=6
-NPHI=6
-NVPAR=10
+NS=5
+NTHETA=5
+NPHI=5
+NVPAR=5
 
 NODES=1
-CORES=12
+CORES=('2' '2' '8')
+
+VMEC="nfp2_QA_cold_high_res_mirror_feas"
+#VMEC="nfp4_QH_cold_high_res"
+#VMEC="nfp4_QH_warm_high_res"
+#VMEC="nfp2_QA_cold_high_res"
+
+
+WARM=("None" "None" "None") # None or filename
+#WARM=("../data/data_opt_nfp2_QA_cold_high_res_mirror_feas_mean_energy_grid_surface_0.3_tmax_0.001_pdfo_mmode_1.pickle" 
+#      "../data/data_opt_nfp2_QA_cold_high_res_mirror_feas_mean_energy_grid_surface_0.5_tmax_0.001_pdfo_mmode_1.pickle" 
+#      "../data/data_opt_nfp2_QA_cold_high_res_mirror_feas_mean_energy_grid_surface_full_tmax_0.001_pdfo_mmode_1.pickle" 
+#      )
+#WARM=("../data/data_opt_nfp4_QH_cold_high_res_mean_energy_grid_surface_0.3_tmax_0.001_cobyla_mmode_1.pickle" 
+#      "../data/data_opt_nfp4_QH_cold_high_res_mean_energy_grid_surface_0.5_tmax_0.001_cobyla_mmode_1.pickle" 
+#      "../data/data_opt_nfp4_QH_cold_high_res_mean_energy_grid_surface_full_tmax_0.001_cobyla_mmode_1.pickle" 
+#      )
+
+
 for idx in ${!SURFS[@]}
 do
   surf=${SURFS[idx]}
   warm=${WARM[idx]}
+  cores=${CORES[idx]}
 
   # make a dir
   dir="_batch_${VMEC}_${OBJECTIVE}_${SAMPLINGTYPE}_surf_${surf}_tmax_${TMAX}_${METHOD}_mmode_${MAXMODE}"
@@ -60,8 +69,8 @@ do
   printf '%s\n' "#SBATCH -o ./job_%j.out    # Name of stdout output file(%j expands to jobId)" >> ${SUB}
   printf '%s\n' "#SBATCH -e ./job_%j.err    # Name of stderr output file(%j expands to jobId)" >> ${SUB}
   printf '%s\n' "#SBATCH -N ${NODES}       # Total number of nodes requested" >> ${SUB}
-  printf '%s\n' "#SBATCH -n ${CORES}       # Total number of cores requested" >> ${SUB}
-  printf '%s\n' "#SBATCH --ntasks-per-node ${CORES}    # Total number of cores requested" >> ${SUB}
+  printf '%s\n' "#SBATCH -n ${cores}       # Total number of cores requested" >> ${SUB}
+  printf '%s\n' "#SBATCH --ntasks-per-node ${cores}    # Total number of cores requested" >> ${SUB}
   printf '%s\n' "#SBATCH --get-user-env     # Tells sbatch to retrieve the users login environment" >> ${SUB}
   printf '%s\n' "#SBATCH -t 96:00:00        # Time limit (hh:mm:ss)" >> ${SUB}
   printf '%s\n' "#SBATCH --mem-per-cpu=4000   # Memory required per allocated CPU" >> ${SUB}
@@ -69,7 +78,7 @@ do
   #printf '%s\n' "#SBATCH --partition=bindel  # Which partition/queue it should run on" >> ${SUB}
   printf '%s\n' "#SBATCH --exclude=g2-cpu-[01-11],g2-cpu-[29-30],g2-cpu-[97-99],g2-compute-[94-97],luxlab-cpu-02" >> ${SUB}
   #printf '%s\n' "#SBATCH --exclusive" >> ${SUB}
-  printf '%s\n' "mpiexec -n $[$NODES*$CORES] python3 optimize.py ${SAMPLINGTYPE} ${surf} ${OBJECTIVE} ${METHOD} ${MAXMODE} ${VMEC} ${warm} ${TMAX} ${NS} ${NTHETA} ${NPHI} ${NVPAR}" >> ${SUB}
+  printf '%s\n' "mpiexec -n $[$NODES*$cores] python3 optimize.py ${SAMPLINGTYPE} ${surf} ${OBJECTIVE} ${METHOD} ${MAXMODE} ${VMEC} ${warm} ${TMAX} ${NS} ${NTHETA} ${NPHI} ${NVPAR}" >> ${SUB}
   
   ## submit
   cd "./${dir}"
