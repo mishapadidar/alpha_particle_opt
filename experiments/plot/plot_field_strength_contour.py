e4e31c205a512cc58663c1de3e6d6adcd9157ba6
mishapadidar <map454@cornell.edu>
2023-02-03 10:18:47 -0500

    finished the field strength contour plotting script

--- experiments/plot/plot_field_strength_contour.py
+++ experiments/plot/plot_field_strength_contour.py
@@ -19,59 +19,64 @@ markers = ['s','o','^','x']
 linestyles=['-','--','-.']
 
 # matplotlib.use('TkAgg')
-# infile ="./configs/data_opt_nfp4_phase_one_aspect_7.0_iota_-1.043_mean_energy_SAA_surface_0.25_tmax_0.01_bobyqa_mmode_3_iota_None.pickle"
-infile ="./configs/data_opt_nfp4_phase_one_aspect_7.0_iota_0.89_mean_energy_SAA_surface_full_tmax_0.01_bobyqa_mmode_3_iota_None.pickle"
+
+#infile ="./nfp4_QH_cold_high_res_phase_one_mirror_1.35_aspect_7.0_iota_-1.043_field_strength.pickle"
+infile ="./nfp4_QH_cold_high_res_phase_one_mirror_1.35_aspect_7.0_iota_1.043_field_strength.pickle"
+#infile ="./nfp4_QH_cold_high_res_phase_one_mirror_1.35_aspect_7.0_iota_0.89_field_strength.pickle"
 
 # load the data
-indata = pickle.load(open(infile,"rb"))
+field_line_data = pickle.load(open(infile,"rb"))
 # load the relevant keys
-field_line_data = indata['field_line_data']
 for key in list(field_line_data.keys()):
     s  = f"{key} = field_line_data['{key}']"
     exec(s)
 
-fig,((ax1,ax2),(ax3,ax4)) = plt.subplots(figsize=(10,10),nrows=2,ncols=2)
+fig,((ax1,ax2),(ax3,ax4)) = plt.subplots(figsize=(8,8),nrows=2,ncols=2)
 
-p1 = ax1.contour(zeta_mesh,theta_mesh,modB_list[0].reshape((nzeta,ntheta)),linewidths=2)
-p2 = ax2.contour(zeta_mesh,theta_mesh,modB_list[1].reshape((nzeta,ntheta)),linewidths=2)
-p3 = ax3.contour(zeta_mesh,theta_mesh,modB_list[2].reshape((nzeta,ntheta)),linewidths=2)
-p4 = ax4.contour(zeta_mesh,theta_mesh,modB_list[3].reshape((nzeta,ntheta)),linewidths=2)
+# plot the data
+p1 = ax1.contour(zeta_mesh,theta_mesh,modB_list[0].reshape((nzeta,ntheta)),levels=13,linewidths=2)
+p2 = ax2.contour(zeta_mesh,theta_mesh,modB_list[1].reshape((nzeta,ntheta)),levels=13,linewidths=2)
+p3 = ax3.contour(zeta_mesh,theta_mesh,modB_list[2].reshape((nzeta,ntheta)),levels=13,linewidths=2)
+p4 = ax4.contour(zeta_mesh,theta_mesh,modB_list[3].reshape((nzeta,ntheta)),levels=13,linewidths=2)
 
-fontsize=20
-t = ax1.text(0.017, 0.93, '$s=$%s'%s_list[0], transform=ax1.transAxes, fontsize=fontsize)
+# make the grey box
+fontsize=18
+t = ax1.text(0.030, 0.915, '$s=$%s'%s_list[0], transform=ax1.transAxes, fontsize=fontsize)
 t.set_bbox(dict(facecolor='grey', alpha=0.6, edgecolor='k'))
-t = ax2.text(1.89, 0.93, '$s=$%s'%s_list[1], transform=ax1.transAxes, fontsize=fontsize)
+t = ax2.text(1.81, 0.915, '$s=$%s'%s_list[1], transform=ax1.transAxes, fontsize=fontsize)
 t.set_bbox(dict(facecolor='grey', alpha=0.6, edgecolor='k'))
-t = ax3.text(0.017, -0.42, '$s=$%s'%s_list[2], transform=ax1.transAxes, fontsize=fontsize)
+t = ax3.text(0.030, -0.38, '$s=$%s'%s_list[2], transform=ax1.transAxes, fontsize=fontsize)
 t.set_bbox(dict(facecolor='grey', alpha=0.6, edgecolor='k'))
-t = ax4.text(1.89, -0.42, '$s=$%s'%s_list[3], transform=ax1.transAxes, fontsize=fontsize)
+t = ax4.text(1.81, -0.38, '$s=$%s'%s_list[3], transform=ax1.transAxes, fontsize=fontsize)
 t.set_bbox(dict(facecolor='grey', alpha=0.6, edgecolor='k'))
 
 # Set the ticks and ticklabels for all axes
-plt.setp([ax1,ax2,ax3,ax4], xticks=[0,np.pi/4,np.pi/2], xticklabels=['0','$\pi$/4','$\pi$/2'],
-        yticks=[0,np.pi,2*np.pi], yticklabels=['0','$\pi$','2$\pi$'])
+plt.setp([ax1,ax2,ax3,ax4], xticks=[0,np.pi/2], xticklabels=['0','$\pi$/2'],
+        yticks=[0,2*np.pi], yticklabels=['0','2$\pi$'])
 
-plt.colorbar(p1,ax=ax1)
-plt.colorbar(p2,ax=ax2)
-plt.colorbar(p3,ax=ax3)
-plt.colorbar(p4,ax=ax4)
+# set the colobar
+from matplotlib import ticker
+tick_font_size = 16
+for ax in (ax1,ax2,ax3,ax4):
+  cbar = plt.colorbar(p1,ax=ax)
+  cbar.locator = ticker.MaxNLocator(nbins=6)
+  cbar.ax.tick_params(labelsize=tick_font_size)
+  cbar.update_ticks()
 
-ax1.set_xlabel('$\zeta$')
-ax2.set_xlabel('$\zeta$')
-ax3.set_xlabel('$\zeta$')
-ax4.set_xlabel('$\zeta$')
-ax1.set_ylabel('$\\theta$')
-ax2.set_ylabel('$\\theta$')
-ax3.set_ylabel('$\\theta$')
-ax4.set_ylabel('$\\theta$')
+# make the axis labels
+for ax in (ax1,ax2,ax3,ax4):
+  ax.set_xlabel('$\zeta$')
+  ax.set_ylabel('$\\theta$')
+  ax.xaxis.set_label_coords(.5,-0.025)
+  ax.yaxis.set_label_coords(-0.025,.5)
 
-ax1.patch.set_edgecolor('black')  
-ax1.patch.set_linewidth('2')  
-ax2.patch.set_edgecolor('black')  
-ax2.patch.set_linewidth('2')  
-ax3.patch.set_edgecolor('black')  
-ax3.patch.set_linewidth('2')  
-ax4.patch.set_edgecolor('black')  
-ax4.patch.set_linewidth('2')  
+# darken the border
+for ax in (ax1,ax2,ax3,ax4):
+  ax.patch.set_edgecolor('black')  
+  ax.patch.set_linewidth('2')  
 
 plt.tight_layout()
+filename = infile[:-7] # remove the .pickle
+filename = filename + ".pdf"
+plt.savefig(filename, format="pdf", bbox_inches="tight")
+#plt.show()
